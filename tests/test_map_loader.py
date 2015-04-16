76b979db57e233dddd8012966ec065e70465ea12
eykd <david.eyk@gmail.com>
2015-04-15 17:33:21 -0700

    Add failing tests for map loader

--- tests/test_map_loader.py
+++ tests/test_map_loader.py
@@ -22,3 +22,19 @@ class MapLoaderTests(TestCase):
         ensure(start_room.exits).is_a(list).of(Exit)
         ensure(start_room.exits[0].target).is_a(Room)
         ensure(start_room.exits[0].target).is_not(start_room)
+
+        room_3 = start_room.exits[1].target
+        ensure(room_3.exits).has_length(4)
+        ensure(room_3.exits).is_a(list).of(Exit)
+
+        room_6 = room_3.exits[2].target
+        ensure(room_6).is_a(Room)
+        ensure(room_6.exits).has_length(2)
+        ensure(room_6.description).equals("A nondescript room")
+
+        room_7 = room_3.exits[3].target
+        ensure(room_7).is_a(Room)
+        ensure(room_7.exits).has_length(2)
+        ensure(room_7.description).equals("A nondescript room")
+
+        ensure(room_6).is_not(room_7)
